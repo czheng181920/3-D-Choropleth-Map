ac07c802188ad4b45feefbf41e7edf20a6e0aab6
Chelsea Zheng <51794314+czheng181920@users.noreply.github.com>
2022-01-09 16:28:00 -0500

    Load in data and make Ohio graph

--- main.R
+++ main.R
@@ -1,3 +1,4 @@
+# clear workspace by doing this: rm(list = ls())
 # Read the shape file with the rgdal library.
 library(rgdal)
 library(rgeos)
@@ -7,7 +8,18 @@ library(broom)
 
 #load Ohio map shapefile
 my_spdf <- readOGR(
-  dsn= "ODOT_County_Boundaries/" ,
+  dsn= "./ODOT_County_Boundaries" ,
   layer="ODOT_County_Boundaries",
   verbose=FALSE
 )
+
+# 'fortify' the data to get a dataframe format required by ggplot2 library(broom)
+spdf_fortified <- tidy(my_spdf, region = "COUNTY_CD")
+#the region is whatever variable in the dataset that displays the county names
+
+# Plot empty map of Ohio to check if it worked library(ggplot2)
+basicPlot = ggplot() +
+  geom_polygon(data = spdf_fortified, aes( x = long, y = lat, group = group),
+               fill="lightblue", color="white", size = 1) +
+  theme_void()
+basicPlot
